e7bdcdb844ddffb59d154c43c58a16fd176c8a37
David Rivera <makutososa300@gmail.com>
2021-03-23 21:10:52 -0400

    Layout

--- src/containers/NotFound.jsx
+++ src/containers/NotFound.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
 import NotFoundMessage from '../components/NotFoundMessage';
 import '../assets/styles/Components/NotFound.scss';
 
 const NotFound = () => (
     <React.Fragment>
-        <Header />
         <NotFoundMessage />
-        <Footer />
     </React.Fragment>
 );
 
