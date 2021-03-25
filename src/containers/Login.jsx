001690cae8a92aefc842e27125913965f078949a
David Rivera <makutososa300@gmail.com>
2021-03-24 23:19:13 -0400

    Login with redux

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,10 +1,12 @@
-import React, { useReducer, useState } from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { loginRequest } from '../actions';
 import '../assets/styles/Components/Login.scss';
 import googleIcon from '../assets/static/google-icon.png';
 import twitterIcon from '../assets/static/twitter-icon.png';
 
-const Login = () => {
+const Login = props => {
     const [form, setValues] = useState({
         email: '',
         password: '',
@@ -19,7 +21,8 @@ const Login = () => {
 
     const handleSubmit = event => {
         event.preventDefault();
-        console.log(form);
+        props.loginRequest(form);
+        props.history.push('/');
     }
 
     return (
@@ -64,4 +67,8 @@ const Login = () => {
     );
 }
 
-export default Login;
+const mapDispatchToProps = {
+    loginRequest,
+};
+
+export default connect(null, mapDispatchToProps)(Login); 
